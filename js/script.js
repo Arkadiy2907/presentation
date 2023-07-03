a4099f45cbea36442ab59ab545f9a151f46863d7
Arkadiy2907 <arkadiy2907@gmail.com>
2023-07-04 00:01:40 +0300

    add pic

--- js/script.js
+++ js/script.js
@@ -23,12 +23,10 @@ const rotateSpeed = -60; // unit: seconds/360 degrees
 const imgWidth = 120; // width of images (unit: px)
 const imgHeight = 170; // height of images (unit: px)
 
-var odrag = document.getElementById('drag-container');
-var ospin = document.getElementById('spin-container');
-var aImg = ospin.getElementsByClassName('spin_img');
-// var aVid = ospin.getElementsByTagName('spin_video');
-// var aEle = [...aImg, ...aVid]; // combine 2 arrays
-var aEle = [...aImg];
+const odrag = document.getElementById('drag-container');
+const ospin = document.getElementById('spin-container');
+const aImg = ospin.getElementsByClassName('spin_img');
+let aEle = [...aImg];
 
 setTimeout(init, 1000);
 
@@ -37,12 +35,12 @@ ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
 // Size of ground - depend on radius
-var ground = document.getElementById('ground');
+const ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
 
 function init(delayTime) {
-  for (var i = 0; i < aEle.length; i++) {
+  for (let i = 0; i < aEle.length; i++) {
     aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
     aEle[i].style.transition = "transform 1s";
     aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
